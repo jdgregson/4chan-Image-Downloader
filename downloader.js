3ffc37823ee62c934b7057b16cde3296db26d79a
DmitriTrofimenko92 <dimaa92@gmail.com>
2019-01-04 02:18:01 -0300

    2018 CORS fix
    Doesnt work because CORS images. Move some download logic to the windows generated by "4chan-image-downloader"

--- downloader.js
+++ downloader.js
@@ -13,11 +13,11 @@
  * @return {Boolean} - whether or not to start the download
  */
 function confirmDownload(count) {
-	let msg = 'Are you sure you want to download all images in this thread? ' +
-	  'There are ' + count + ' images to download.\n\n It may slow down your ' +
-	  'browser while the downloads are in progress.\n\n You may be prompted to ' +
-	  'allow the downloads at the top of the page a few times.';
-	return confirm(msg);
+    let msg = 'Are you sure you want to download all images in this thread? ' +
+        'There are ' + count + ' images to download.\n\n It may slow down your ' +
+        'browser while the downloads are in progress.\n\n You may be prompted to ' +
+        'allow the downloads at the top of the page a few times.';
+    return confirm(msg);
 }
 
 /**
@@ -25,33 +25,49 @@ function confirmDownload(count) {
  * @param  {Boolean} confirmed - whether or not to treat this call as pre-
  *   confirmed and bypass the user's confirmation
  */
-function downloadImages(confirmed=false) {
-  let images = document.getElementsByClassName("fileThumb");
-	if(confirmed || confirmDownload(images.length)) {
-		for(let i=0; i<images.length; i++) {
-			if(images[i].href) {
-				// clone the image without 4chan X's event listeners
-				let oldImg = images[i];
-				let newImg = oldImg.cloneNode(true);
-				oldImg.parentNode.replaceChild(newImg, oldImg);
+function wait(ms) {
+    var d = new Date();
+    var d2 = null;
+    do {
+        d2 = new Date();
+    }
+    while (d2 - d < ms);
+}
 
-				// set up and trigger the download of the image
-				let name = newImg.href.split('/').slice(-1)[0];
-				let link = newImg.href + '?4chan-image-downloader';
-				newImg.href = link;
-				newImg.setAttribute("download", name);
-				newImg.click();
-				newImg.removeAttribute("download");
-				newImg.click();
+function downloadImages(confirmed = false) {
+    let images = document.getElementsByClassName("fileThumb");
+    if (confirmed || confirmDownload(images.length)) {
+        for (let i = 0; i < images.length; i++) {
+            if (images[i].href) {
+                // clone the image without 4chan X's event listeners
+                let oldImg = images[i];
+                let newImg = oldImg.cloneNode(true);
+                oldImg.parentNode.replaceChild(newImg, oldImg);
 
-				// put 4chan X's image back so the user can click on it again
-				newImg.parentNode.replaceChild(oldImg, newImg);
-			}
-		}
-	}
+                // set up and trigger the download of the image
+                let name = newImg.href.split('/').slice(-1)[0];
+                let link = newImg.href + '?4chan-image-downloader';
+                newImg.href = link;
+                newImg.setAttribute("download", name);
+                wait(500);
+                newImg.click()
+                // put 4chan X's image back so the user can click on it again
+                newImg.parentNode.replaceChild(oldImg, newImg);
+
+            }
+        }
+    }
 }
 
 // if we're running on a new tab that we opened, try to close it every second
-if(document.location.search.indexOf('4chan-image-downloader') > -1) {
-	self.setInterval(function(){close()}, 1000);
+if (document.location.search.includes('4chan-image-downloader')) {
+    var url = document.getElementsByTagName("img")[0].getAttribute("src").replace("?4chan-image-downloader", "");
+    var filename = url.split('/').slice(-1)[0];
+    var newAnchor = document.createElement("a");
+    newAnchor.href = url;
+    newAnchor.setAttribute("download", filename);
+    newAnchor.click();
+    self.setInterval(function() {
+        close()
+    }, 2000);
 }
